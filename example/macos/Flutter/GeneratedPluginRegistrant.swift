d12d2f29a82e742d7e43a9739962bc04e8eddd94
Khamidjon <hamidovhamid1998@gmail.com>
2021-12-25 17:38:55 +0500

    convert int32 to int64 to match color

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import fluttter_socket_log_plugin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FluttterSocketLogPlugin.register(with: registry.registrar(forPlugin: "FluttterSocketLogPlugin"))
 }
