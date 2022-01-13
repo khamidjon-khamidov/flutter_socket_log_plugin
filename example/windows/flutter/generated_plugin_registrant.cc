7d3fd696e969b3ba77b399063057d49074a7953a
Khamidjon <hamidovhamid1998@mail.ru>
2022-01-13 10:50:49 +0500

    run publish dry run

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <fluttter_socket_log_plugin/fluttter_socket_log_plugin.h>
 #include <network_info_plus_windows/network_info_plus_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FluttterSocketLogPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FluttterSocketLogPlugin"));
   NetworkInfoPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("NetworkInfoPlusWindowsPlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  fluttter_socket_log_plugin
   network_info_plus_windows
 )
 
